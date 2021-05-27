a4618367550143f44885b6ab144beb50cc10344b
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-05-27 17:00:12 -0300

    Finalizado a parte inicial do footer e adiciona o mesmo a página Home

--- src/views/pages/Home/index.tsx
+++ src/views/pages/Home/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import Header from '../../../components/Header';
+import Footer from '../../../components/Footer';
+
+import { HomeComponent } from "./styles";
 
 const Home: React.FC = () => {
 
   return (
-    <>
+    <HomeComponent>
       <Header />
-    </>
+      <h1>Home</h1>
+      <Footer />
+    </HomeComponent>
   );
 }
 
---
+++ src/views/pages/Home/styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const HomeComponent = styled.div`
+  height: 100vh;
+`;
