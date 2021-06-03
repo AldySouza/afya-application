b2893a9160181fe1ae3a413f355969dfa617877e
brenogcota <brenocota4@gmail.com>
2021-06-03 17:43:42 -0300

    fix: slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -5,15 +5,17 @@ import { Wrapper, Container, Item } from './styles';
 const Slider: React.FC = () => {
   const [current, setCurrent] = useState(1);
 
-  const handleSlide = () => {
+  const handleSlidePrev = () => {
       current != 1 ? setCurrent(current-1) : setCurrent(6);
-      current != 6 ? setCurrent(current+1) : setCurrent(1);
+  }
 
+  const handleSlideNext = () => {
+      current != 6 ? setCurrent(current+1) : setCurrent(1);
   }
   return (
       <Wrapper>
         <Container>
-            <button onClick={handleSlide}>
+            <button onClick={handleSlidePrev}>
                 <svg width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.67 1.77L9.9 0L0 9.9L9.9 19.8L11.67 18.03L3.54 9.9L11.67 1.77Z" fill="#D40054D40054"/>
                 </svg>
@@ -48,7 +50,7 @@ const Slider: React.FC = () => {
             >
                 <img src="https://s3-sa-east-1.amazonaws.com/medcel.static/images/afya/unidades-logos/logos-2021/ipemed.svg" alt="" />
             </Item>
-            <button onClick={handleSlide}>
+            <button onClick={handleSlideNext}>
                 <svg width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0.0271879 18.0279L1.76978 19.8249L11.8207 10.0782L2.07405 0.0272061L0.277062 1.7698L8.28117 10.0238L0.0271879 18.0279Z" fill="#D40054D40054"/>
                 </svg>
