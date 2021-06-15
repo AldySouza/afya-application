cfbd5f9685e654d1400e552c3f32f4f890b419d9
Eduardo Silva <69769953+EduardoRS78@users.noreply.github.com>
2021-06-15 10:51:21 -0400

    criando paginas do cliente

---
+++ src/views/pages/Register clients/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container } from './styles';
+import Button from '../../../components/Button';
+import Input from '../../../components/Input';
+
+import { ReactComponent as LogoXG } from '../../../assets/logo-xg.svg';
+
+
+const Register: React.FC = () => {
+  const [form] = useState({});
+
+  // const handleForm = (e: any) => {
+  //   e.preventDefault();
+
+  //   const { username, email, password } = document.forms[0];
+
+  //   console.log(username.value, email.value, password.value);
+  // }
+
+  return (
+    <Container>
+
+      <div className="leftContainer">
+        <form name="signin" className="signinForm">
+          <LogoXG />
+          <Input inputType="text" placeHolder="Digite seu nome completo" Name="name" />
+          <Input inputType="text" placeHolder="Digite um username" Name="username" />
+          <Input inputType="email" placeHolder="Digite seu e-mail" Name="email" />
+          <Input inputType="email" placeHolder="Digite confirme seu e-mail" Name="confirm-email" />
+          <Input inputType="password" placeHolder="Digite uma senha" Name="password" />
+          <Input inputType="password" placeHolder="Digite confirme sua senha" Name="password" />
+          <div className="checkbox-type">
+            <input type="checkbox" name="cliente" id="cliente" />Cliente
+            <input type="checkbox" name="especialista" id="especialista" />Especialista
+          </div>
+
+          <Link to="/registro-cliente">
+            <Button>Próximo</Button>
+          </Link>
+
+          <span>Já tem cadastro? <Link to="/login"><strong>Entrar</strong></Link></span>
+        </form>
+      </div>
+
+      <div className="rightContainer">
+        <h1>
+          Prencha os campos <br />
+          e faça parte dessa <br />
+          comunidade
+        </h1>
+        <LogoXG />
+      </div>
+    </Container>
+  );
+}
+
+export default Register;
