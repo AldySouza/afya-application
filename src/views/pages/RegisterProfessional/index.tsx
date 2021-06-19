708e95369beffc56b40d0861d195f7c81dffc3e1
brenogcota <brenocota4@gmail.com>
2021-06-19 12:58:16 -0300

    refact: professional register refactor

--- src/views/pages/RegisterProfessional/index.tsx
+++ src/views/pages/RegisterProfessional/index.tsx
@@ -1,37 +1,75 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
 import { Container } from './styles';
 import Button from '../../../components/Button';
 import Input from '../../../components/Input';
 
 import { ReactComponent as LogoXG } from '../../../assets/logo-xg.svg';
+import AuthContext from '../../../context/AuthContext';
+import Validator from '../../../helpers/Validator';
 
 const RegisterProfessional: React.FC = () => {
-  const [form] = useState({});
+  const [error, setError] = useState({
+      message: '',
+      isError: false
+  });
+  const [isDone, setIsDone] = useState<boolean>(false);
+  const [client, setClient] = useState({});
+  const { user } = useContext(AuthContext);
 
-  // const handleForm = (e: any) => {
-  //   e.preventDefault();
-  
-  //   const { username, email, password } = document.forms[0];
+  const handleForm = (e: any) => {
+      e.preventDefault();
+      const { crm, celular, phone, profession } = document.forms[0];
 
-  //   console.log(username.value, email.value, password.value);
-  // }
-  
+      if(Validator.isEmpty([crm.value, celular.value, phone.value, profession.value])) {
+        const error = {error: 'Verifique se preencheu todos os campos', isError: true };
+        handleError(error);
+        return;
+      }
+
+      setClient({
+        cpf: crm.value, celular: celular.value, 
+        telefone: phone.value, profissao: profession.value, 
+        name: user.name, email: user.username,
+        id: user.id
+      });
+
+      setIsDone(true);
+  }
+
+  const handleError = (body: any) => {
+    setError({message: body.error, isError: true });
+
+    setTimeout(() => {
+        setError({message: '', isError: false });
+    }, 3000)
+  }
   return (
     <Container>
 
       <div className="leftContainer">
         <form name="signin" className="signinForm">
           <LogoXG />
+          { error.isError  && <span className="error">{error.message}</span> }
           <Input inputType="text" placeHolder="Digite seu CRM" Name="crm" />
           <Input inputType="text" placeHolder="Digite seu celular" Name="celular" />
           <Input inputType="text" placeHolder="Digite um telefone" Name="phone" />
           <Input inputType="text" placeHolder="Digite sua profissão" Name="profession" />
 
-          <Link to="/registro-endereco">
-            <Button>Próximo</Button>
-          </Link>
+          {
+            isDone ?
+            <Redirect
+                to={{
+                pathname: "/registro-endereco",
+                state: { prevData: client }
+              }}
+            />
+            :
+            <span onClick={(e) => handleForm(e)}>
+              <Button>Próximo</Button>
+            </span>
+          }
 
           {/* <span>Já tem cadastro? <Link to="/login"><strong>Entrar</strong></Link></span> */}
         </form>
