b8d6a5dabe7e64d6dacdd15faac12a614fe50a88
brenogcota <brenocota4@gmail.com>
2021-06-21 22:31:59 -0300

    feat: list specilists

--- src/views/pages/Home/index.tsx
+++ src/views/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from '../../../components/Header';
 import Footer from '../../../components/Footer';
@@ -7,8 +7,34 @@ import Partner from './components/Partner';
 import Schedules from './components/Schedules';
 
 import { HomeComponent } from "./styles";
+import { AxiosHttpClient } from '../../../helpers/httpClient/ajaxAdapter';
+import { baseURL } from '../../../services/Utils';
+
+interface ISpecialist {
+  celular: string,
+  email: string,
+  id: string,
+  name: string,
+  registro: string,
+  telefone: string
+}
 
 const Home: React.FC = () => {
+  const [specialists, setSpecialists] = useState<ISpecialist[]>()
+  useEffect(() => {
+    getSpecialists(); 
+  }, []);
+
+  const getSpecialists = async () => {
+    const http = new AxiosHttpClient();
+
+    const {body, statusCode } = await http.request({
+      url: `${baseURL}/specialists`,
+      method: 'get'
+    });
+
+    statusCode == 200 ? setSpecialists(body) : setSpecialists([]);
+  }
 
   return (
     <HomeComponent>
@@ -16,8 +42,14 @@ const Home: React.FC = () => {
       <section>
         <FindSpecialist />
         <Partner />
-        <Schedules title="Cardiologista" value={355} />
-        <Schedules title="Cardiologista" value={755}/>
+        {
+          specialists && specialists?.length > 0 ?
+            specialists.map(specialist => {
+              return <Schedules title={specialist.name} value={360}/>
+            })
+          :
+          <h1>Nenhum especialista cadastrado ainda</h1>
+        }
       </section>
       <Footer />
     </HomeComponent>
--- src/views/pages/Search/index.tsx
+++ src/views/pages/Search/index.tsx
@@ -7,17 +7,43 @@ import Card from './components/Card';
 import { Container, SearchBar } from './styles';
 import { ReactComponent as SearchIcon } from '../../../assets/search.svg';
 import Pin from '../../../assets/pin.svg';
+import { baseURL } from '../../../services/Utils';
+import { AxiosHttpClient } from '../../../helpers/httpClient/ajaxAdapter';
+
+
+interface ISpecialist {
+  celular: string,
+  email: string,
+  id: string,
+  name: string,
+  registro: string,
+  telefone: string
+}
 
 const Search: React.FC = () => {
   const [query, setQuery] = useState(''); 
   const [hash, setHash] = useState('');
 
+  const [specialists, setSpecialists] = useState<ISpecialist[]>()
+
   useEffect(() => {
       const hash = window.location.hash;
       const query = window.location.search;
       
-      console.log(hash, query)
+      console.log(hash, query);
+      getSpecialists(); 
   }, [])
+
+  const getSpecialists = async () => {
+    const http = new AxiosHttpClient();
+
+    const {body, statusCode } = await http.request({
+      url: `${baseURL}/specialists`,
+      method: 'get'
+    });
+
+    statusCode == 200 ? setSpecialists(body) : setSpecialists([]);
+  }
 
   return (
       <Container>
@@ -33,9 +59,14 @@ const Search: React.FC = () => {
 
           <main className="_main-container">
             <div>
-              <Card name="Marcos Alves" profession="Ortopedista" address="Rua Carlos Prates" />
-              <Card name="Douglas Morais" profession="Cardiologista" address="Rua Carlos F. Costa" />
-              <Card name="Breno Cota" profession="Cardiologista" address="Rua Carlos F. Costa" />
+            {
+          specialists && specialists?.length > 0 ?
+            specialists.map(specialist => {
+              return <Card name={specialist.name} profession="Ortopedista" address="Rua Carlos Prates" />
+              })
+              :
+              <h1>Nenhum especialista cadastrado ainda</h1>
+            }
             </div>
           </main>
               
